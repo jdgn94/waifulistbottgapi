4e9eb8195037748c36bc1f501ce619c96c26b014
Juan Daniel González Noriega <jdgn94@gmail.com>
2022-02-06 13:22:41 -0400

    eliminacion de carpeta imnecesaria

--- src2/index.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV !== 'producction') require('dotenv').config();
-
-const app = require('./app');
-
-app.listen(app.get('port'), () => {
-  console.log('server on port', app.get('port'));
-})
